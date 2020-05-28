13418f13ae86dc02487bdd85d48edffad4db46dd
wildwing <ggipk1010@gmail.com>
2020-05-28 21:56:28 +0900

    너는 쥬드하다

--- src/App.vue
+++ src/App.vue
@@ -13,6 +13,8 @@
           src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
           transition="scale-transition"
           width="40"
+
+          
         />
 
         <v-img
